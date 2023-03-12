6d729401aaf6a35733d0cffbf7d425d2bf303e81
Yifeng Liu <liuyifeng2020@hotmail.com>
2023-03-12 06:15:17 -0700

    update

--- trimapGen.py
+++ trimapGen.py
@@ -1,24 +1,28 @@
 import cv2 as cv
 import numpy as np
 
-image_path  = str(input('give a path of your alpha image (png format required):'))
-mask = cv.imread(image_path, 0)
+def trimapGenerator():
+    alpha_path  = str(input('give a path (png format required):'))
+    trimap_path = alpha_path[:len(alpha_path)-9] + '_trimap.png'
+    mask = cv.imread(alpha_path, 0)
+    mask = cv.resize(mask, (480,640))
 
-kernel_dilate = np.ones((3, 3))
-dilate = cv.dilate(mask,kernel_dilate, iterations=6)
+    kernel_dilate = np.ones((3, 3))
+    dilate = cv.dilate(mask,kernel_dilate, iterations=3)
 
-kernel_erode = np.ones((3, 3))
-erode  = cv.erode(mask,kernel_erode, iterations=10)
+    kernel_erode = np.ones((3, 3))
+    erode  = cv.erode(mask,kernel_erode, iterations=5)
 
-mask[mask==1] = 255
-gray = cv.bitwise_xor(erode,dilate)
-gray[gray!=0]=105
+    mask[mask==1] = 255
+    gray = cv.bitwise_xor(erode,dilate)
+    gray[gray!=0]=105
 
-for i in range(erode.shape[0]):
-    for j in range(erode.shape[1]):
-        if(erode[i][j] != 0):
-            gray[i][j] = 255
+    for i in range(erode.shape[0]):
+        for j in range(erode.shape[1]):
+            if(erode[i][j] != 0):
+                gray[i][j] = 255
 
-cv.imwrite('trimap.png',gray)
-cv.imshow('trimp',gray)
-cv.waitKey(0)
+    cv.imwrite(trimap_path, gray)
+
+if __name__ == "__main__":
+    trimapGenerator()
